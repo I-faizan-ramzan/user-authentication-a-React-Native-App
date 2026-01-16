4d830479e136101190f44e0c28d6cb809c435097
I-faizan-ramzan <faizan.ramzan36@yahoo.com>
2026-01-16 16:03:14 +0500

    adding latest

--- constants/styles.js
+++ constants/styles.js
@@ -1,7 +1,7 @@
 export const Colors = {
-  primary100: '#f9beda',
-  primary500: '#c30b64',
-  primary800: '#610440',
-  error100: '#fcdcbf',
-  error500: '#f37c13',
-}
+  primary100: "#7696a9",
+  primary500: "#557392",
+  primary800: "#042231",
+  error100: "#fcdcbf",
+  error500: "#f37c13",
+};
